47dc76a7900ef8f75d6f8afeeec01556bd3e5ca4
Хмыров А.М <a.hmyrov-saratov@mail.ru>
2023-08-09 22:01:29 +0400

    add: final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let subString = str.split('');
+    check: 
+    for (let i = 0; i <= subString.length; ) {
+
+        if (subString.length === 0) {
+          return true;
+        }
+
+        if (subString.length === 1) {
+          return false;
+        }
+
+        if (subString.length > 1) {
+            for (let j = 0; j < bracketsConfig.length; j++) {
+                if (subString[i] === bracketsConfig[j][0] && subString[i+1] === bracketsConfig[j][1]) {
+                    subString.splice(i,2);
+                    i = 0;
+                    continue check;
+                }
+            }
+        }
+        i++
+    }
+    return false;
 }
